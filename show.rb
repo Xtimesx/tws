dac78f505799a29cfbdb5c15f1d0cdea8481bc49
Hauke Lehmann <h.lehmann@brandslisten.com>
2016-10-06 12:20:27 +0200

    Add full test search to show.rb

--- show.rb
+++ show.rb
@@ -22,6 +22,15 @@ def user_search(username)
   #order_by(Sequel.desc(:status__db_id)).limit(10)
 end
 
+def text_search(text)
+  puts "text: \"#{text}\""
+  DB.from(:status).
+  left_join(:media, :source_status_id => :status__id).
+  where(Sequel.like(:status__text, "%#{text}%")).
+  group_by(:status__id).
+  order_by(Sequel.desc(:status__db_id)).limit(50)
+end
+
 puts 'tag to search in database'
 search = gets.chomp 
 dataset = case search[0]
@@ -29,12 +38,14 @@ dataset = case search[0]
     tag_search(search[1,search.length])
   when '@'
     user_search(search[1,search.length])
+  else
+    text_search(search)
 end
 puts dataset.sql
 dataset.each do |r|
   #puts r.inspect
   #puts r[:local_path].inspect
-  file = !!r[:local_path] ? "file://#{File.expand_path(r[:local_path])}" : ""
+  file = !!r[:local_path] ? "file://#{File.expand_path("./assets/" +  r[:local_path])}" : ""
   puts "#{r[:text]}: #{file}"
   puts "#{r[:name]}/#{r[:screen_name]}"
 end
