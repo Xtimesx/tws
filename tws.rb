b28b3ff263cc450ca0f219287549866e832f0fbe
Nanok Bartolomäus Lee <nanoklee@gmail.com>
2017-05-03 09:24:56 +0200

    Revert "remove comment stuff"
    This reverts commit 9b67c49f356c51aa302a968827e89ec2f0ff4ec2.

--- tws.rb
+++ tws.rb
@@ -89,7 +89,66 @@ end
 #  show_with_context status, depth: 10
 #end
 
+#def download(status)
+#  threads = []
+#  threads << Thread.new(status) do |status|
+#    status.media.each do |m|
+#      m.download(folder: status.id.to_s);
+#      m.save;
+#      Thread.current[:path] = "file://#{File.expand_path(m.file_path)}"
+#    end
+#  end
+#  download_to_asset_server(status)
+#end
 
+# REMOVE ME
+def download(status)
+  threads = []
+  status.media.each do |m|
+    threads << Thread.new(status) do |status|
+      uniq_path= (status.retweeted? ? status.retweeted_status.id : status.id).to_s(16).upcase.scan(/../).join('/')
+      uniq_name= "#{uniq_path}/#{m.file_name}"
+      uri = URI("http://127.0.0.1:8080")
+      Net::HTTP.start(uri.host, uri.port) do |http|
+        request = Net::HTTP::Post.new uri
+        media_uri = case m
+          when Twitter::Media::Video
+            m[:video_info][:variants].select{|v| v[:content_type] == 'video/mp4'}.sort{|b,a| a[:bitrate] <=> b[:bitrate]}.first[:url]
+          else
+            m.media_uri
+          end
+        request.body = "{ \"job_iD\": #{rand(2**30)}, \"uniq_name\": \"#{uniq_name}\", \"src\": \"#{media_uri}\" }"
+        response = http.request request # Net::HTTPResponse object
+        Thread.current[:code] = response.code
+      end
+      Thread.current[:uri] = uri.to_s + '/' + uniq_name
+      Thread.current[:path] = "file://#{File.expand_path("./assets/" + m.file_path(folder: uniq_path))}"
+      m.save
+    end
+  end
+
+  threads.each do |t|
+    t.join
+    puts "#{t[:code]} for: #{t[:path]}"
+  end
+end
+
+def show(status, options= {})
+  msg = ['']
+  case status.class.name
+  when 'Twitter::Tweet'
+    msg << (puts status.full_text.gsub('&gt;','>').gsub('&lt;','<').gsub('&amp;','&') )
+    say(status.full_text, {lang: status.lang, voice: voiceForUserByName(status.user.screen_name) }) unless status.retweet? || !@speak
+  when 'Twitter::User'
+    msg << (puts "#{status.screen_name}/#{status.name}: #{voiceForUserByName(status.screen_name)}")
+    msg << (puts status.description)
+    msg << (puts status.location)
+  else
+    puts status.class.name
+    status.inspect
+  end
+  msg.join("\r\n")
+end
 
 def show_with_context(status, options= {})
   msg = ['']
