beeeb23a324203c298def679f95b9f25167b7c73
Xtimesx <h.lehmann@brandslisten.com>
2018-03-28 14:11:55 +0200

    Add files via upload

---
+++ socket_server.rb
@@ -0,0 +1,111 @@
+#!/usr/bin/env ruby -w
+require "socket"
+DB = {} #dummy
+require "bunny"
+require 'json'
+require 'twitter'
+require './twitter_patches'
+
+# Copyright: Hiroshi Ichikawa <http://gimite.net/en/>
+# Lincense: New BSD Lincense
+
+def init_bunny
+
+  end
+
+$LOAD_PATH << File.dirname(__FILE__) + "/../lib"
+require "web_socket"
+require "thread"
+
+Thread.abort_on_exception = true
+
+if ARGV.size != 2
+  $stderr.puts("Usage: ruby sample/chat_server.rb ACCEPTED_DOMAIN PORT")
+  exit(1)
+end
+
+server = WebSocketServer.new(
+  :accepted_domains => [ARGV[0]],
+  :port => ARGV[1].to_i())
+puts("Server is running at port %d" % server.port)
+connections = []
+
+server.run() do |ws|
+  begin
+    
+    puts("Connection accepted")
+    ws.handshake()
+    que = Queue.new()
+    connections.push(que)
+    
+    thread = Thread.new() do
+      while true
+        message = que.pop()
+        ws.send(message)
+        puts("Sent: #{message}")
+      end
+    end
+
+    @bunny = Bunny.new(:automatically_recover => false)
+    @bunny.start
+
+    ch_in  = @bunny.create_channel
+    x = ch_in.topic("tweets")
+    @q = ch_in.queue("", :exclusive => true)
+
+    @q.bind(x, routing_key: '#')
+
+    @q.subscribe(:block => true) do |delivery_info, properties, body|
+      for conn in connections
+        conn.push(body)
+      end
+    end
+ 
+    while data = ws.receive()
+      puts("Received: #{data}")
+
+    end
+    
+  ensure
+    connections.delete(que)
+    thread.terminate() if thread
+    puts("Connection closed")
+  end
+end
+
+
+
+class Server
+  def initialize( port, ip )
+    @server = TCPServer.open( ip, port )
+    @connections = Hash.new
+    @rooms = Hash.new
+    @clients = Hash.new
+    @connections[:server] = @server
+    @connections[:rooms] = @rooms
+    @connections[:clients] = @clients
+    @q = nil
+    run
+
+  end
+
+  
+
+  def run
+    loop {
+      Thread.start(@server.accept) do | client |
+        nick_name = client.gets.chomp.to_sym
+        @connections[:clients].each do |other_name, other_client|
+          if nick_name == other_name || client == other_client
+            client.puts "This username already exist"
+            Thread.kill self
+          end
+        end
+        puts "#{nick_name} #{client}"
+        @connections[:clients][nick_name] = client
+      end
+    }.join
+  end
+end
+
+Server.new( 3000, "localhost" )
